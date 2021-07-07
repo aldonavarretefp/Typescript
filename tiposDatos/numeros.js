1eed708d6f24393a14307268e08dc570dd2a8ca7
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-07 14:21:29 -0500

    Numeros

--- tiposDatos/numeros.js
+++ tiposDatos/numeros.js
@@ -1,1 +1,13 @@
 "use strict";
+var avengers = 5;
+var villanos;
+var otros = 2;
+if (avengers > villanos) {
+    console.log("Estamos salvados");
+}
+else {
+    console.log("Estamos muertos");
+}
+otros = 123;
+otros = 123.123;
+// otros = '123'
