a596102609df2d4209c859f1c270e3ca90cad53b
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-07 15:00:33 -0500

    Void

--- tiposDatos/void.js
+++ tiposDatos/void.js
@@ -1,1 +1,7 @@
 "use strict";
+//La funcion regresa un undefined 
+function llamarBatman() {
+    console.log("Mostrar Batisenal");
+}
+var retorno = llamarBatman();
+console.log(retorno);
---
+++ tiposDatos/void.ts
@@ -0,0 +1,7 @@
+//La funcion regresa un undefined 
+function llamarBatman():void{
+    console.log("Mostrar Batisenal")
+}
+
+let retorno = llamarBatman();
+console.log(retorno)
