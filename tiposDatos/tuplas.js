16a10736efcde95c8e089d3fff46504dce319dce
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-07 14:43:28 -0500

    Tuplas

--- tiposDatos/tuplas.js
+++ tiposDatos/tuplas.js
@@ -1,1 +1,7 @@
 "use strict";
+var heroe = ['Dr.Strange', 100, true];
+// heroe[0] = 123;
+// heroe[1] = "IronMan"
+console.log(heroe);
+heroe.push(true);
+console.log(heroe);
---
+++ tiposDatos/tuplas.ts
@@ -0,0 +1,7 @@
+let heroe:[string,number,boolean] = ['Dr.Strange',100,true]
+// heroe[0] = 123;
+// heroe[1] = "IronMan"
+console.log(heroe)
+
+heroe.push(true)
+console.log(heroe)
