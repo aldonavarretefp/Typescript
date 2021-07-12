e08eee09b75e440187795189b2a2ef3e81036720
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-12 14:07:23 -0500

    Null y Undefined

---
+++ tiposDatos/NullyUndefined.ts
@@ -0,0 +1,14 @@
+let nada:undefined = undefined;
+console.log(nada)
+nada= null;
+console.log(nada)
+
+let ironman:string;
+// "strictNullChecks": false --> Si me deja hacer ese cambio
+console.log(ironman)
+ironman = "Tony";
+console.log(ironman)
+ironman = null;
+console.log(ironman)
+
+//Los numeros y los strings pueden tomar valores de null y undefined
