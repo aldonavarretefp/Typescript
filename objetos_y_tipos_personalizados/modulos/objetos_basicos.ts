ec58d164d13f2a208b8b8f083d19afd6493a693e
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-13 19:06:26 -0500

    Objetos Basicos

---
+++ objetos_y_tipos_personalizados/modulos/objetos_basicos.ts
@@ -0,0 +1,14 @@
+export = flash;
+let flash = {
+    nombre:"Barry Allen",
+    edad: 24,
+    poderes: ["Puede correr muy rapido","Viajar por el tiempo"]
+}
+console.log(flash)
+
+flash = {
+    nombre: "Clark Kent",
+    edad:500,
+    poderes:["Poder volar"]
+}
+console.log(flash)
