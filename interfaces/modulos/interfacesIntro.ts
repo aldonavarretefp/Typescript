b3c3d1ad176dc421a720f5464a97d22606ff9851
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-22 23:12:58 -0500

    Interfaces

---
+++ interfaces/modulos/interfacesIntro.ts
@@ -0,0 +1,20 @@
+interface Xmen {
+    nombre:string;
+    //Parametros opcionales
+    regenerar?(nombreReal:string):void;
+}
+
+function enviarMision( xmen : Xmen ){
+    console.log(`Enviando a: ${xmen.nombre}`);
+    xmen.regenerar("Logan");
+}
+
+let wolverine:Xmen = {
+    nombre:"Wolverine",
+    //Aqui especificamos la funcion de la interfaz
+    regenerar(x:string){
+        console.log(`Se ha regenerado a ${x}`);
+    }
+}
+
+enviarMision(wolverine);
