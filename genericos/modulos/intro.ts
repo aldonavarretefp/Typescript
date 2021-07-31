4df24a9cc102ae87f64a63d98924abe9614ba6fa
Aldo Navarrete <aaldiitoo@gmail.com>
2021-07-31 00:04:55 -0500

    Ejemplo 2 con funciones genericas

--- genericos/modulos/intro.ts
+++ genericos/modulos/intro.ts
@@ -6,3 +6,25 @@ function regresar<T>(arg:T) {
 console.log(regresar(12.5345345).toFixed(2));
 console.log(regresar("Aldo Navarrete").length);
 console.log(regresar(new Date().toLocaleTimeString()));
+
+// Ejemplo 2:
+
+
+function funcionGenerica<T>(arg:T) {
+    return arg;
+}
+
+type Heroe = {
+    nombre: string,
+    nombreReal:String,   
+}
+type Villano = {
+    nombre: string,
+    apellido?:string,
+    nombreReal:String,   
+}
+const aldo = {
+    nombre:'Master Y ',
+    nombreReal:'Aldo Navarrete'
+}
+console.log(funcionGenerica<Villano>(aldo));
